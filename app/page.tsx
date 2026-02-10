b08ac1d84640733886765707db216860616355c3
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-02-10 21:30:59 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { dbConnect } from "./dbconnection";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const dbInfo = await dbConnect();
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center">
       <ul>
         {dbInfo.map((data) => (
           <li key={data.id}>{data.name}</li>
