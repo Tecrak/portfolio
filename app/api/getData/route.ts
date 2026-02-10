f950a06e6f58a1497fcc633d59857dcc31a64217
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-02-11 00:57:30 +0100

    commit

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -2,7 +2,7 @@ import { dbConnect } from "@/app/dbconnection";
 
 export async function GET() {
   const data = await dbConnect({
-    query: `SELECT id, name FROM ${process.env.DB_TABLE}`,
+    query: `SELECT * FROM ${process.env.DB_TABLE}`,
   });
   return new Response(JSON.stringify(data), { status: 200 });
 }
