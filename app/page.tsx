36a91886a3bb84f970cdbd8e19478a99ba14cb30
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-02-10 20:55:55 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import { dbConnect } from "./dbconnect";
 
-export default function Home() {
+export default async function Home() {
+  const dbInfo = await dbConnect();
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <ul>
+        {dbInfo.map((data) => (
+          <li key={data.id}>{data.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
