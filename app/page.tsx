4735d0cff42af0c3b67407ece5dfa5a553f1cd44
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-04-01 22:12:25 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <div className="main">dasd</div>;
+  return (
+    <div className="main">
+      <div className="main_content">
+        <h1>Stanislav's Portfolio</h1>
+      </div>
+    </div>
+  );
 }
