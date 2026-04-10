a97ff82c0b165d380970cc0895103fd3c1e38608
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-04-11 01:42:45 +0200

    testing block position

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,39 @@ export default function Page() {
   return (
     <div className="main">
       <div className="main_content">
-        <h1>About me</h1>
-        <div className=""></div>
+        <div className="aboutInfoContainer">
+          <div className="aboutImage">
+            <img src="https://imgs.search.brave.com/Ev4_obH611_BnAFAAjqlcnMldOvlwQtOibG5JJCaDMY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL2Nj/OHlOR2tjZDQ2N2lm/OUoxTDdpcFVNQThH/bGtFcDhRY3ljZ21N/MC1vaTlOdVg3dmM4/VWVVNW1jNFZIdS1K/NTlKdmwtQTN2TTY4/UjFNQVNiVFZyTTh0/X3NjQmRwN29VcEFj/Q2hSdzQ5MDJFYV9j/M1g9dzE0NDAtaDgx/MC1uLW51"></img>
+          </div>
+          <div className="shortInfoAbout">
+            <div className="detailedInfo">
+              <div className="aboutMe">
+                <h4>Shorty about me</h4>
+                <p>
+                  Einstieg als Junior-Webentwickler (Frontend / Full-Stack) in
+                  einem Unternehmen, in dem ich meine Kenntnisse in JavaScript,
+                  React, HTML und CSS einsetzen und meine Fähigkeiten in der
+                  modernen Webentwicklung weiterentwickeln kann.
+                </p>
+              </div>
+              <div className="languageSkills">
+                <h4>Languages</h4>
+                <p>
+                  Ukrainisch | Muttersprache
+                  <br></br>
+                  Englisch | Fortgeschritten. Ich hatte Kurse am College, die
+                  dazu beitrugen, mein Sprachniveau zu verbessern
+                  <br></br>
+                  Deutch | Grundkenntnisse. Derzeit studiere ich die Sprache und
+                  besuche aktiv Deutschkurse.
+                </p>
+              </div>
+            </div>
+            <div className="contactInfo">
+              <p>Contact info</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div
         className="welcomeWindow"
