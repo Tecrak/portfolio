f9089c3963be1deef27c50b02c8c3b0c9a5afb4f
Tecrak <87039557+Tecrak@users.noreply.github.com>
2026-04-21 00:55:28 +0200

    Styling education section

--- app/[locale]/components/education.tsx
+++ app/[locale]/components/education.tsx
@@ -42,6 +42,24 @@ export default function Education() {
                 </p>
               </div>
             </div>
+            <div className="education_item">
+              <div className="education_header">
+                <h3>2222Level 3 BTEK Computer Science (Level in EQF:4)</h3>
+                <h4>SEP 2022- JUN 2024</h4>
+              </div>
+              <div className="education_place">
+                <h4>Stockport College</h4>
+              </div>
+              <div className="education_marks">Abschlussnote: D*DD</div>
+              <div className="education_desc">
+                <p>
+                  Im Rahmen dieses Kurses wurden folgende Technologien und
+                  Themen behandelt: Java, HTML / HTML5, HTML Scripting, PHP,
+                  MySQL / SQL, Cybersecurity und Cybersecurity-Risiken,
+                  Cascading Style Sheets (CSS) und CSS Flexbox, JavaScript
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </BlockToClose>
