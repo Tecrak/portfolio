b8c0a486536474c7f6ab7985fd351cf774e35f23
Stas <stasponomarchuk@gmail.com>
2026-05-07 18:38:05 +0200

    first phase
    blocks position

--- app/[locale]/page/examples/layout.tsx
+++ app/[locale]/page/examples/layout.tsx
@@ -4,13 +4,56 @@ import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { EXAMPLELINKS, ExampleLink } from "@/app/config/examplelinks";
-import "./styles/examples.layout.css";
+import "./styles/layout.css";
 
 export default function ExamplesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return;
-  <></>;
+  return (
+    <div className="mainBox">
+      <div className="sideBar">
+        <h4>Навчики</h4>
+        <ul>
+          <li>
+            <div className="sideBarText">
+              <p>PostgreSQL</p>
+            </div>
+            <div className="sideBarLed">
+              <span></span>
+            </div>
+          </li>
+          <li>
+            <div className="sideBarText">
+              <p>PostgreSQL</p>
+            </div>
+            <div className="sideBarLed">
+              <span></span>
+            </div>
+          </li>
+          <li>
+            <div className="sideBarText">
+              <p>PostgreSQL</p>
+            </div>
+            <div className="sideBarLed">
+              <span></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="mainArea">
+        <div className="pathText">
+          <h4>Skills /</h4>
+        </div>
+        <div className="exmplDesc">
+          <p>ddsdfdsfsdfsdfsdfsdfsdfsdf</p>
+        </div>
+        <div className="testAreaBlock">
+          <p>TEST AREA</p>
+          <div className="testArea">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/[locale]/page/examples/page.tsx
+++ app/[locale]/page/examples/page.tsx
@@ -1,3 +1,3 @@
 export default function ExamplesIndexPage() {
-  return null;
+  return <div>dasidhjasmrfiuewhn</div>;
 }
