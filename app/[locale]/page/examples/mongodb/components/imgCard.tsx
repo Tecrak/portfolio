fa1bc6045cb6b7cfbb3b8e19212cf1cccaeeac63
Stas <stasponomarchuk@gmail.com>
2026-05-18 23:53:45 +0200

    small change

---
+++ app/[locale]/page/examples/mongodb/components/imgCard.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useState } from "react";
+import { mData } from "../config/data";
+
+export default function ImgCard() {
+  const [imgOpened, setImgOpened] = useState(0);
+  function selectedImg(imgID: number) {
+    const valid = imgID === imgOpened;
+    return valid;
+  }
+
+  return (
+    <ul className="imgList">
+      {mData.map((data) => (
+        <li key={data._id}>
+          <div className="comOpenned imgBox">
+            <img
+              src={data.imgSrc}
+              onClick={() =>
+                imgOpened <= 0 ? setImgOpened(data._id) : setImgOpened(0)
+              }></img>
+            {/* <div className="deleteBox"> */}
+            <div className="bttnSection">
+              <div className="likeBox">
+                <button className="likeBttn notLiked">❤️ Like</button>
+                <span className="commsCount">{data.likeCount}</span>
+              </div>
+              <ul className="commentsBttn">
+                <span>💬</span>
+                {data.comments.map((cData) => (
+                  <li
+                    key={`${cData.aId}+"a"`}
+                    className="commentBlock"
+                    style={
+                      selectedImg(data._id)
+                        ? { display: "block" }
+                        : { display: "none" }
+                    }>
+                    <div className="commentTopPart">
+                      <img src={data.imgSrc}></img>
+                      <p className="commentName">{}</p>
+                      <p className="commentDate">22.2.2222</p>
+                    </div>
+                    <div className="commentContent">
+                      <p>{cData.comment}</p>
+                    </div>
+                  </li>
+                ))}
+                <div
+                  className="newCommentBox"
+                  style={
+                    selectedImg(data._id)
+                      ? { display: "block" }
+                      : { display: "none" }
+                  }>
+                  <input type="text"></input>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- app/[locale]/page/examples/mongodb/page.tsx
+++ app/[locale]/page/examples/mongodb/page.tsx
@@ -1,76 +1,19 @@
 "use client";
 
 import "./styles/page.css";
-import { mData } from "./config/data";
 import { useMongopeople, MongoPerson } from "./api/useMPeople";
-import { useState } from "react";
+import ImgCard from "./components/imgCard";
 
 export default function MongoDBPage() {
   // const { data: people = [], isLoading, isError } = useMongopeople();
 
   // if (isLoading) return <div className="pg-loading">завантаження...</div>;
   // if (isError) return <div className="pg-loading">помилка підключення</div>;
-  const [imgOpened, setImgOpened] = useState(0);
-
-  function selectedImg(imgID: number) {
-    const valid = imgID === imgOpened;
-    return valid;
-  }
 
   return (
     <div className="mainMPage">
       <button className="newPhotoBttn">Add new</button>
-      <ul className="imgList">
-        {mData.map((data) => (
-          <li key={data._id}>
-            <div className="comOpenned imgBox">
-              <img
-                src={data.imgSrc}
-                onClick={() =>
-                  imgOpened <= 0 ? setImgOpened(data._id) : setImgOpened(0)
-                }></img>
-              {/* <div className="deleteBox"> */}
-              <div className="bttnSection">
-                <div className="likeBox">
-                  <button className="likeBttn notLiked">❤️ Like</button>
-                  <span className="commsCount">{data.likeCount}</span>
-                </div>
-                <ul className="commentsBttn">
-                  <span>💬</span>
-                  {data.comments.map((cData) => (
-                    <li
-                      key={`${cData.aId}+"a"`}
-                      className="commentBlock"
-                      style={
-                        selectedImg(data._id)
-                          ? { display: "block" }
-                          : { display: "none" }
-                      }>
-                      <div className="commentTopPart">
-                        <img src={data.imgSrc}></img>
-                        <p className="commentName">{}</p>
-                        <p className="commentDate">22.2.2222</p>
-                      </div>
-                      <div className="commentContent">
-                        <p>{cData.comment}</p>
-                      </div>
-                    </li>
-                  ))}
-                  <div
-                    className="newCommentBox"
-                    style={
-                      selectedImg(data._id)
-                        ? { display: "block" }
-                        : { display: "none" }
-                    }>
-                    <input type="text"></input>
-                  </div>
-                </ul>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <ImgCard />
     </div>
   );
 }
