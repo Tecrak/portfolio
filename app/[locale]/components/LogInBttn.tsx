3e18a04e3854af40101b3f37872bbc30cabef2bf
Stas <stasponomarchuk@gmail.com>
2026-05-31 18:53:37 +0200

    Basic logInBttn styling

--- app/[locale]/components/LogInBttn.tsx
+++ app/[locale]/components/LogInBttn.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import "./styles/LogInBttn.css";
 
 export default function AuthButton() {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <div>
+      <div className="loggedInBlock">
         <Image
           src={session.user?.image ?? ""}
           width={32}
@@ -21,5 +22,14 @@ export default function AuthButton() {
     );
   }
 
-  return <button onClick={() => signIn("google")}>Sign in with Google</button>;
+  return (
+    <button className="logInBttn" onClick={() => signIn("google")}>
+      <img
+        alt="noLogin"
+        src="https://img.icons8.com/?size=100&id=118880&format=png&color=000000"
+        width={32}
+        height={32}
+      />
+    </button>
+  );
 }
